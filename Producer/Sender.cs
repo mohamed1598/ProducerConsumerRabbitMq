6f66827d30a9efeaf65a67396948e41655a8caee
mohamed elshafey <elshafeym170@gmail.com>
2024-04-06 20:59:32 +0200

    create basic console app producer

---
+++ Producer/Sender.cs
@@ -0,0 +1,13 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("BasicTest", false, false, false, null);
+
+string message = "Getting started with .net core RabbitMQ";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("","BasicTest",null,body);
+Console.WriteLine("Sent message : {0}...",message);
